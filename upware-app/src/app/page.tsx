473c9d45e46547455f4b96cbc95655b1e86c2c5b
Reinawan <admin@reinawan.com>
2024-02-06 15:21:45 +0700

    style: footer

--- upware-app/src/app/page.tsx
+++ upware-app/src/app/page.tsx
@@ -1,4 +1,6 @@
 import Banner from "@/components/banner";
+import DetailInformasi from "@/components/detailinformasi";
+import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
 
 export default function Home() {
@@ -6,6 +8,22 @@ export default function Home() {
     <>
       <Navbar />
       <Banner />
+      <div className="flex w-full px-16 mt-14 mb-10">
+        <div className="flex-1 pe-10 items-center">
+          <DetailInformasi />
+        </div>
+        <div className="flex-1">
+          <div>
+            <img
+              src="https://awsimages.detik.net.id/community/media/visual/2023/04/12/tupperware.jpeg?w=600&q=90"
+              width={700}
+              height={700}
+              className="rounded-2xl"
+            />
+          </div>
+        </div>
+      </div>
+      <Footer />
     </>
   );
 }
---
+++ upware-app/src/components/detailinformasi.tsx
@@ -0,0 +1,14 @@
+export default function DetailInformasi()  {
+	return (
+		<div className="flex justify-start items-center w-full h-full">
+			<div className="flex flex-col justify-start py-8 px-4 text-left">
+				<h1 className="mb-4 text-6xl font-extrabold tracking-tight leading-none">
+					UpWare
+				</h1>
+				<p className="mb-8 text-lg font-normal lg:text-xl">
+                Selamat datang di UpWare! Tempatnya untuk segala kebutuhan peralatan rumah tangga berkualitas tinggi yang akan membuat hidup Anda lebih mudah dan nyaman.
+				</p>
+			</div>
+		</div>
+	);
+};

bc1b79285cffb467679b3e288edfc465c7726435
Reinawan <admin@reinawan.com>
2024-02-08 16:10:22 +0700

    fix: Reoute validasi register

--- upware-app/src/db/models/user.ts
+++ upware-app/src/db/models/user.ts
@@ -2,13 +2,14 @@ import { ObjectId } from "mongodb";
 import { database } from "../config/mongodb";
 import { hashText } from "../helpers/hash";
 import { z } from "zod";
+import { NextResponse } from "next/server";
 
 const userSchema = z.object({
     name: z.string(),
     username: z.string(),
     email: z.string().email(),
     password: z.string().min(5)
-})
+});
 
 export interface UserInterface {
     _id: ObjectId;
@@ -18,7 +19,7 @@ export interface UserInterface {
     password: string;
 };
 
-type NewInput = Omit<UserInterface, "_id">
+type NewInput = Omit<UserInterface, "_id">;
 
 class UserModel {
     static getCollection() {
@@ -26,20 +27,45 @@ class UserModel {
     }
 
     static async getAll() {
-        return await this.getCollection().find().project({password:0}).toArray() as UserInterface[];
+        return await this.getCollection().find().project({ password: 0 }).toArray() as UserInterface[];
     }
 
     static async getById(id: string) {
         return await this.getCollection().findOne({
             _id: new ObjectId(id)
-        }, {projection: {password: 0}}) as UserInterface | null;
+        }, { projection: { password: 0 } }) as UserInterface | null;
+    }
+
+    static async getUserByUsername(username: string) {
+        return await this.getCollection().findOne({
+            username: username
+        }) as UserInterface | null;
+    }
+
+    static async getUserByEmail(email: string) {
+        return await this.getCollection().findOne({
+            email: email
+        }) as UserInterface | null;
     }
 
     static async register(newUser: NewInput) {
-        const parsed = userSchema.safeParse(newUser);
-        if(!parsed.success) {
-            throw parsed.error
+        const existingUserByUsername = await this.getUserByUsername(newUser.username);
+        const existingUserByEmail = await this.getUserByEmail(newUser.email);
+
+        if (existingUserByUsername || existingUserByEmail) {
+            return NextResponse.json(
+                {
+                    message: "Username or email already exists"
+                },
+                { status: 400 }
+            )
         }
+
+        const parsed = userSchema.safeParse(newUser);
+        if (!parsed.success) {
+            throw parsed.error;
+        }
+
         const result = await this.getCollection().insertOne({
             ...newUser, password: hashText(newUser.password)
         });
@@ -51,4 +77,4 @@ class UserModel {
     }
 }
 
-export default UserModel
+export default UserModel;

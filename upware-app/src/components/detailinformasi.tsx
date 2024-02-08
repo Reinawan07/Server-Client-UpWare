7e4d9ea67f581a5879ffd41bd00f9535496298a5
Reinawan <admin@reinawan.com>
2024-02-09 01:45:04 +0700

    style: style logo

--- upware-app/src/components/detailinformasi.tsx
+++ upware-app/src/components/detailinformasi.tsx
@@ -3,7 +3,7 @@ export default function DetailInformasi()  {
 		<div className="flex justify-start items-center w-full h-full">
 			<div className="flex flex-col justify-start py-8 px-4 text-left">
 				<h1 className="mb-4 text-6xl font-extrabold tracking-tight leading-none">
-					UpWare
+				<b className="text-primary">Up</b><b className="text-accent">Ware</b>
 				</h1>
 				<p className="mb-8 text-lg font-normal lg:text-xl">
                 Selamat datang di UpWare! Tempatnya untuk segala kebutuhan peralatan rumah tangga berkualitas tinggi yang akan membuat hidup Anda lebih mudah dan nyaman.
--- upware-app/src/components/navbar.tsx
+++ upware-app/src/components/navbar.tsx
@@ -25,7 +25,7 @@ export default function Navbar() {
         <>
             <div className="navbar bg-base-100 border-b shadow-xl mb-5 sticky top-0 z-50">
                 <div className="navbar-start">
-                    <Link href="/" className="hover:text-primary font-bold focus:outline-none focus:shadow-outline transition duration-300 text-2xl ml-4"><b>UpWare</b></Link>
+                    <Link href="/" className="hover:text-primary font-bold focus:outline-none focus:shadow-outline transition duration-300 text-2xl ml-4"><b className="text-primary">Up</b><b className="text-accent">Ware</b></Link>
                 </div>
                 <div className="navbar-center gap-7">
                     <Link href="/product" className="hover:text-primary font-bold focus:outline-none focus:shadow-outline transition duration-300">Product</Link>
